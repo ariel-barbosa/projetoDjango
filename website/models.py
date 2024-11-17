c47f45de1ec1ba03e2fddad5bea267cf0755e00d
Ariel Barbosa Santos <153242199+ariel-barbosa@users.noreply.github.com>
2024-11-17 12:49:15 -0300

    commit

--- website/models.py
+++ website/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 # Modelos
-class Funcionarios(models.MOdel):
+class Funcionarios(models.Model):
     nome = models.CharField(max_length=255, null=False, blank=False)
     sobrenome = models.CharField(max_length=255, null=False, blank=False)
     cpf = models.CharField(max_length=255, null=False, blank=False)
-    tempo_de_serviço = models.IntegerField(default=0, null=False, blank=False)
+    tempo_de_serviço = models.PositiveBigIntegerField(default=0, null=False, blank=False)
     remuneracao = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False)
 
+    # O campo objetos = models.Manager() é utilizado para fazer operações de busca
+    objetos = models.Manager()
+
+    # Portanto, toda vez que você alterar um arquivo de modelo, não se
+    # esqueça de executar python manage.py makemigrations!
 
